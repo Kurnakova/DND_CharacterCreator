d456340654df946c0c751f0cbb29ebe3a3d23069
Kurnakova <87601316+Kurnakova@users.noreply.github.com>
2024-05-22 10:18:20 +0300

    Update Ancestry.cs
    Changed names of ancestry bonuses

--- Ancestry.cs
+++ Ancestry.cs
@@ -3,12 +3,12 @@ class Ancestry
     static int lastID = 0;
     public int _ID { get; } = 0;
     public string _name { get; } = "undefined ancestry";
-    public int _strBonus { get; } = 0;
-    public int _dexBonus { get; } = 0;
-    public int _conBonus { get; } = 0;
-    public int _intBonus { get; } = 0;
-    public int _wisBonus { get; } = 0;
-    public int _chaBonus { get; } = 0;
+    public int _strengthBonus { get; } = 0;
+    public int _dexterityBonus { get; } = 0;
+    public int _constitutionBonus { get; } = 0;
+    public int _intelligenceBonus { get; } = 0;
+    public int _wisdomBonus { get; } = 0;
+    public int _charismaBonus { get; } = 0;
 
     public Ancestry( string name, int strBonus = 0, int dexBonus = 0, int conBonus = 0, int intBonus = 0, int wisBonus = 0, int chaBonus = 0 )
     {
@@ -16,32 +16,32 @@ class Ancestry
         lastID++;
         _ID = lastID;
         _name = name;
-        _strBonus = strBonus;
-        _dexBonus = dexBonus;
-        _conBonus = conBonus;
-        _intBonus = intBonus;
-        _wisBonus = wisBonus;
-        _chaBonus = chaBonus;
+        _strengthBonus = strBonus;
+        _dexterityBonus = dexBonus;
+        _constitutionBonus = conBonus;
+        _intelligenceBonus = intBonus;
+        _wisdomBonus = wisBonus;
+        _charismaBonus = chaBonus;
     }
     public Ancestry (int type)
     {
         if (type == 1)
         {
             _name = "Эльф";
-            _dexBonus = 2;
-            _wisBonus = 1;
+            _dexterityBonus = 2;
+            _wisdomBonus = 1;
         }
         else if (type == 2)
         {
             _name = "Дварф";
-            _strBonus = 1;
-            _conBonus = 2;
+            _strengthBonus = 1;
+            _constitutionBonus = 2;
         }
         else if (type == 3)
         {
             _name = "Гном";
-            _dexBonus = 1;
-            _intBonus = 2;
+            _dexterityBonus = 1;
+            _intelligenceBonus = 2;
         }
     }
 }
