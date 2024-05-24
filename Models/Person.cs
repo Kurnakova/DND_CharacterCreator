c1d43791e1c0cd72fcd35cadc56c402fff856a43
Kurnakova <87601316+Kurnakova@users.noreply.github.com>
2024-05-24 16:13:47 +0300

    Deleted method SetCharacteristic

--- Models/Person.cs
+++ Models/Person.cs
@@ -15,21 +15,16 @@ class Person
         Console.WriteLine($"Имя: {_name}, Происхождение: {_ancestry.Name}");
         Console.WriteLine($"Сила: {_strength.value} ({_strength.GetModifierString()}), Ловкость:{_dexterity.value} ({_dexterity.GetModifierString()}), Телосложение: {_constitution.value} ({_constitution.GetModifierString()}), Интеллект: {_intelligence.value} ({_intelligence.GetModifierString()}), Мудрость: {_wisdom.value} ({_wisdom.GetModifierString()}), Харизма: {_charisma.value} ({_charisma.GetModifierString()})");
     }
-    int SetCharacteristic(int number, int ancestryBonus)
-    {
-        int characteristic = number + ancestryBonus;
-        return characteristic;
-    }
-   
+    
     public Person (string name, Ancestry ancestry, int strength, int dexterity, int constitution, int intelligence, int wisdom, int charisma)
     {
         _name = name;
         _ancestry = ancestry;
-        _strength = new Characteristic (SetCharacteristic(strength, ancestry.StrengthBonus));
-        _dexterity = new Characteristic (SetCharacteristic(dexterity, ancestry.DexterityBonus));
-        _constitution = new Characteristic (SetCharacteristic(constitution, ancestry.ConstitutionBonus));
-        _intelligence = new Characteristic (SetCharacteristic(intelligence, ancestry.IntelligenceBonus));
-        _wisdom = new Characteristic (SetCharacteristic(wisdom, ancestry.WisdomBonus));
-        _charisma = new Characteristic (SetCharacteristic(charisma, ancestry.CharismaBonus));
+        _strength = new Characteristic (strength + ancestry.StrengthBonus);
+        _dexterity = new Characteristic ((dexterity + ancestry.DexterityBonus));
+        _constitution = new Characteristic (constitution + ancestry.ConstitutionBonus);
+        _intelligence = new Characteristic (intelligence + ancestry.IntelligenceBonus);
+        _wisdom = new Characteristic (wisdom + ancestry.WisdomBonus);
+        _charisma = new Characteristic (charisma + ancestry.CharismaBonus);
     }
 }
