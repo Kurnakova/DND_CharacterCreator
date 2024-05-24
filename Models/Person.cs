f93cdd3721b470bde166393faf680814633d95f5
Kurnakova <87601316+Kurnakova@users.noreply.github.com>
2024-05-24 16:07:48 +0300

    Move class Characteristic to a separate file

--- Models/Person.cs
+++ Models/Person.cs
@@ -20,28 +20,7 @@ class Person
         int characteristic = number + ancestryBonus;
         return characteristic;
     }
-    private class Characteristic
-    {
-        public int value { get; set; } = 0;
-        public int GetModifierInt()
-        {
-            decimal modifierDecimal = (Math.Floor((decimal)(value - 10)/2));
-            int modifier = Convert.ToInt32(modifierDecimal);
-            return modifier;
-        }
-        public string GetModifierString()
-        {
-            int modifier = GetModifierInt();
-            if (modifier > 0)
-                return $"+{modifier}";
-            else
-                return $"{modifier}";
-        }
-        public Characteristic (int value)
-        {
-            this.value = value;
-        }
-    }
+   
     public Person (string name, Ancestry ancestry, int strength, int dexterity, int constitution, int intelligence, int wisdom, int charisma)
     {
         _name = name;
