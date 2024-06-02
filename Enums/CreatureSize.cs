cbcf74a880e761b6f892fa21bf51c686f1e22ae2
kurnakovv <maksk210@gmail.com>
2024-06-02 20:29:51 +0900

    Add numeration for CreatureSize

--- Enums/CreatureSize.cs
+++ Enums/CreatureSize.cs
@@ -2,10 +2,10 @@ namespace DND_CharacterCreator.Enums;
 
 public enum CreatureSize
 {
-    Tiny,
-    Small,
-    Medium,
-    Large,
-    Huge,
-    Gargantuan,
+    Tiny = 1,
+    Small = 2,
+    Medium = 3,
+    Large = 4,
+    Huge = 5,
+    Gargantuan = 6,
 }
